e16fd53291341be5fd6c01aec8f48e787baa3c86
xonmello <43194417+xonmello@users.noreply.github.com>
2019-11-20 18:43:48 -0700

    Solved with env

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	for i in 0..args.len() {
+		if args[i].parse::<u64>().is_ok() {
+			println!("{:?}", hailstone(args[i].parse::<u64>().unwrap()));
+		}
+	}
+}
+
+fn 	hailstone(mut num: u64) -> Vec<u64> {
+	let mut list: Vec<u64> = vec!();
+	list.push(num);
+	while num != 1 {
+		if num % 2 == 0 {
+			num /= 2;
+			list.push(num);
+		} else {
+			num = (num * 3) + 1;
+			list.push(num);
+			num /= 2;
+			list.push(num);
+		}
+	}
+	list
+}
